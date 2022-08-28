1c35664cab18467f1e09041eccdb043c736474e5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-28 14:29:39 +0300

    Added modules dir

---
+++ nodeSection/2-modules/2-names-module.js
@@ -0,0 +1,11 @@
+// local
+const secret = "SUPER SECRED DATA";
+
+// export
+const john = "John";
+const peter = "Peter";
+
+module.exports = {
+  john,
+  peter,
+};
---
+++ nodeSection/2-modules/modules.js
@@ -0,0 +1,20 @@
+// CommonJS, every file is module(by default)
+// Node.js has a set of built-in modules which you can use without any further installation.
+// Example: OS, PATH, FS, HTTP
+
+// Modules - Encapsulated Code (only share minimum). 
+// Consider modules to be the same as JavaScript libraries.
+// A set of functiA set of functions you want to include in your application.ons you want to include in your application.
+
+const names = require('./2-names-module')
+const sayHi = require('./5-util-modules')
+
+const data = require('./6-alternative-flavour')
+console.log(data) // { items: [ 'item1', 'item2' ], singlePerson: { name: 'Bob' } }
+
+
+sayHi("Susan");
+sayHi(names.john);
+sayHi(names.peter);
+
+require('./7-mind-grenade')
