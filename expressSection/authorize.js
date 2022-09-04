dfa8e9f3ec49a1c14f8c95e529f67d26f440b1ff
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-04 17:13:19 +0300

    Edited authorize.js

--- expressSection/authorize.js
+++ expressSection/authorize.js
@@ -1,7 +1,13 @@
 const authorize = (req, res, next) => {
-  console.log("Authorize");
-  next();
+  const {user} = req.query;
+
+  if(user === 'john') {
+    req.user = {name: 'john', id: 3}
+    next()
+  } else {
+    res.status(401).send('Unauthorized!')
+  }
 };
 // This is not the way we aouthorize osers in express
-
+// IRL we will check if token exist
 module.exports = authorize
