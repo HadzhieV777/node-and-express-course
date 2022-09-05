f6f2f8d15748e249a1971f7c961525d2de087159
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-05 21:29:43 +0300

    Edited people.js

--- expressSection/routes/people.js
+++ expressSection/routes/people.js
@@ -1,55 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  getPeople,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people.js");
 
-app.get("/api/people", (req, res) => {
-    res.status(200).json({ success: true, data: people });
-  });
-  
-  app.post("/api/people", (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-      return res
-        .status(400)
-        .json({ success: false, msg: "Please provide name value" });
-    }
-    res.status(201).json({ success: true, person: name });
-  });
-  
-  app.put("/api/people/:id", (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-  
-    const person = people.find((person) => person.id === Number(id));
-  
-    if (!person) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `There is no person with id ${id}.` });
-    }
-  
-    const editedPerson = people.map((person) => {
-      if (person.id === Number(id)) {
-        person.name = name;
-      }
-      return person;
-    });
-    res.status(200).json({ success: true, data: editedPerson });
-  });
-  
-  app.delete("/api/people/:id", (req, res) => {
-    const person = people.find((person) => person.id === Number(req.params.id));
-  
-    if (!person) {
-      return res.status(404).json({
-        success: false,
-        msg: `There is no person with id ${req.params.id}.`,
-      });
-    }
-  
-    const editedPeople = people.filter(
-      (person) => person.id !== Number(req.params.id)
-    );
-    return res.status(200).json({ success: true, data: editedPeople });
-  });
-  
+router.get("/", getPeople);
+router.post("/", createPerson);
+router.put("/:id", updatePerson);
+router.delete("/:id", deletePerson);
+
+// We also can define routes with chaining
+// router.route('/').get(getPeople)
+
+module.exports = router;
